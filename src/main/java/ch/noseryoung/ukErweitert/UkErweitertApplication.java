f8657511025d80246aa704a2783935e7d61bf7bb
AriixIT <49060231+AriixIT@users.noreply.github.com>
2020-04-15 10:24:41 +0200

    added project with travis

---
+++ src/main/java/ch/noseryoung/ukErweitert/UkErweitertApplication.java
@@ -0,0 +1,13 @@
+package ch.noseryoung.ukErweitert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UkErweitertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UkErweitertApplication.class, args);
+	}
+
+}
